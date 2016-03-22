6aae56c3d21dc79fc4e478f238a057b2cbca7425
Spencer Sellers <spencesellers@gmail.com>
2016-03-22 13:54:16 -0500

    Added a canvas

--- client/index.js
+++ client/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     console.log("Hello World");
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext('2d');
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
 });
